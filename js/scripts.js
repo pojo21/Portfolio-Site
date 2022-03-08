21a161cfaf6b665c4a0389c733b6ec78fa345dd3
Alex Poulos <tboard9@gmail.com>
2022-03-08 16:25:25 -0700

    Lint Fixes v1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 console.log("Hello World")
 
-function menuToggle() {
+function menuToggle () {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
